6141c5da0f694ce1537374a4748843c7a3114124
Keita Sakurai <keitasakuraims@gmail.com>
2023-08-23 16:18:55 +0900

    Added commentout

--- test_func_param.py
+++ test_func_param.py
@@ -1,23 +1,18 @@
 import pytest
 
 
-def addition(x, y):
-    """
-    足し算をして、返す
-    """
+def addition(x, y): # 足し算をして、返す
     return x + y
-
 
 # テスト関数のパラメータ化
 @pytest.mark.parametrize(
-    "x, y , result",
+    # x, y , result
     [   (1, 2, 3),
-        (1, 2, 4), #
+        (1, 2, 4),
         (10, 23, 33)
     ],
 )
+
+# 関数をパラメータ化して、additionを検証
 def test_addition(x, y, result):
-    """
-    関数をパラメータ化して、additionを検証
-    """
     assert result == addition(x, y)
