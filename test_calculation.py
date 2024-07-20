be3999ced5b0daea03a8274446721e64a2a12145
keita-sa <keitasakuraims@gmail.com>
2024-07-20 16:45:20 +0900

    Added pytest

--- test_calculation.py
+++ test_calculation.py
@@ -2,7 +2,16 @@ import pytest
 import calculation
 
 class TestCal(object):
+    def test_add_num_and_double(self):
+        cal = calculation.Cal()
+        assert cal.add_num_and_double(1, 1) == 4
 
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            cal = calculation.Cal()
+            cal.add_num_and_double('1', '1')
+
+"""        
     @classmethod
     def setup_class(cls):
         cls.cal = calculation.Cal()
@@ -15,3 +24,4 @@ class TestCal(object):
         elif os_name == 'windows':
             print('dir')
         assert self.cal.add_num_and_double(1, 1) == 4
+"""
