1d1b103430f824cf489b2b8d6d905d3cf833f82a
keita-sa <keitasakuraims@gmail.com>
2024-07-21 10:08:32 +0900

    setup and teardown

--- calculation.py
+++ calculation.py
@@ -6,3 +6,12 @@ class Cal(object):
         result = x + y
         result *= 2
         return result
+
+"""
+    def save(self, dir_path, file_name):
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+        file_path = os.path.join(dir_path, file_name)
+        with open(file_path, 'w') as f:
+            f.write('test')
+"""
--- test_calculation.py
+++ test_calculation.py
@@ -1,21 +1,32 @@
+import os
 import pytest
 import calculation
 
+# setup and teardown
 class TestCal(object):
+    def setup_method(self, method):
+        print('method={}'.format(method.__name__))
+        self.cal = calculation.Cal()
+
+    def teardown_method(self, method):
+        print('method={}'.format(method.__name__))
+        del self.cal
+
     def test_add_num_and_double(self):
         cal = calculation.Cal()
-        assert cal.add_num_and_double(1, 1) == 4
+        assert self.cal.add_num_and_double(1, 1) == 4
 
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
-            cal = calculation.Cal()
-            cal.add_num_and_double('1', '1')
+            self.cal.add_num_and_double('1', '1')
 
-"""        
-    @classmethod
-    def setup_class(cls):
-        cls.cal = calculation.Cal()
 
+"""
+    def test_save(self, tmpdir):
+        self.cal.save(tmpdir, self.test_file_name)
+        test_file_path = os.path.join(tmpdir, self.test_file_name)
+        assert os.path.exists(test_file_path) is True
+            
     def test_add_num_and_double(self, request):
         os_name = request.config.getoption('--os-name')
         print(os_name)
